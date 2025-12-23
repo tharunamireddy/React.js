12bfe2960dc31898a7213eb818889a66b3738972
tharunamireddy <108273258+tharunamireddy@users.noreply.github.com>
2025-12-23 12:52:04 +0530

    third commit

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,10 +1,34 @@
-function Counter(){
-    return(
-        <div>
-            <h1>Counter</h1>
-            <p>0</p>
-        </div>
+import React, { Component } from 'react'
+
+class Counter extends Component {
+
+    state = {counter:0}
+
+    inc =()=>{
+        this.setState(prev=>({counter:prev.counter+1}))
+        this.setState(prev=>({counter:prev.counter+1}))
+    }
+
+    re = ()=>{
+        this.setState({counter:0})
+    }
+
+    dec =()=>{
+        this.setState(prev=>({counter:prev.counter-1}))
+        this.setState(prev=>({counter:prev.counter-1}))
+    }
+
+  render() {
+    return (
+      <div>
+        <h2>Counter Component</h2>
+        <p>{this.state.counter}</p>
+        <button onClick={this.dec}>-</button>
+        <button onClick={this.re}>Reset</button>
+        <button onClick={this.inc}>+</button>
+      </div>
     )
+  }
 }
 
-export default Counter;
+export default Counter
