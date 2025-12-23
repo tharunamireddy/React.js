de29e812729400ad374ee44d1bbe09310742a95d
tharunamireddy <108273258+tharunamireddy@users.noreply.github.com>
2025-12-23 13:03:26 +0530

    fifth commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Counter from './Components/Counter'
 import Navbar from './Components/Navbar'
+import Home from './Components/Home'
+import Cart from './Components/Cart'
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Home/>
       <Counter/>
+      <Cart/>
     </>
   )
 }
